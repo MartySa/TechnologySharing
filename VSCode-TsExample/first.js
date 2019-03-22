1011140a1637cc504347031da6cf2c8d021dff32
MartySa <93066194@qq.com>
2019-03-22 18:05:41 +0800

    TS学习更新

---
+++ VSCode-TsExample/first.js
@@ -0,0 +1,96 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+function sayHello(person) {
+    return "hello," + person;
+}
+var user = "MartySa";
+console.log(sayHello(user));
+var bDone = false; // bool值
+var createNewBool = new Boolean(false); // bool对象
+var a = 1;
+var b = 0x88;
+var c = 8;
+var d = 8;
+var f = NaN;
+var g = Infinity;
+var Name = "MartySa";
+var Age = 18;
+var totalStr = "Hello, My name is " + Name + ". I'll be " + (Age - 1) + " years old. "; // 格式化字符串 ` 与 $
+function alertName() {
+    alert('My Name is MartySa.');
+}
+var unusable = undefined;
+var u;
+// let u1 : number = u;
+// let strNumber : string = "Seven";
+// strNumber = 0x01
+var anyThing = "Hello";
+console.log(anyThing.Name);
+console.log(anyThing.Name.firstName);
+var something; // 该版本和书上有差异，如果不显示调用 any 后面的setName会报错
+something = "Seven";
+something = 7;
+something = "9";
+something.setName("MartySa");
+var unionVal;
+unionVal = "Seven";
+unionVal = 7;
+//unionVal = false;
+function GetLenth(something) {
+    return something.toString();
+}
+// 类 门
+var Door = /** @class */ (function () {
+    function Door() {
+    }
+    Door.prototype.ShowName = function () {
+    };
+    return Door;
+}());
+// 多重继承
+var SecurityDoor = /** @class */ (function (_super) {
+    __extends(SecurityDoor, _super);
+    function SecurityDoor() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    SecurityDoor.prototype.fuc = function () {
+    };
+    return SecurityDoor;
+}(Door));
+var door = new SecurityDoor; // 不确定是否这样初始化
+door.fuc();
+// 汽车，实现接口 Alarm和Light
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.lightON = function () {
+    };
+    Car.prototype.lightOff = function () {
+    };
+    Car.prototype.fuc = function () {
+    };
+    return Car;
+}());
+var Car2 = /** @class */ (function () {
+    function Car2() {
+    }
+    Car2.prototype.fuc = function () {
+    };
+    Car2.prototype.lightOff = function () {
+    };
+    Car2.prototype.lightOn = function () {
+    };
+    return Car2;
+}());
---
+++ VSCode-TsExample/first.ts
@@ -0,0 +1,130 @@
+function sayHello(person : string)
+{
+    return "hello," + person;
+}
+
+let user = "MartySa";
+
+console.log(sayHello(user));
+
+let bDone : boolean = false;                        // bool值
+let createNewBool : Boolean = new Boolean(false);   // bool对象
+let a : number = 1;
+let b : number = 0x88;
+let c : number = 0b1000;
+let d : number = 0o10;
+let f : number = NaN;
+let g : number = Infinity;
+
+let Name : string = "MartySa";
+let Age : number = 18;
+let totalStr : string = `Hello, My name is ${Name}. I'll be ${Age - 1} years old. `;    // 格式化字符串 ` 与 $
+
+function alertName() : void 
+{
+    alert('My Name is MartySa.');
+}
+
+let unusable : void = undefined;
+let u : undefined;
+// let u1 : number = u;
+// let strNumber : string = "Seven";
+// strNumber = 0x01
+
+let anyThing : any = "Hello";
+console.log(anyThing.Name);
+console.log(anyThing.Name.firstName);
+
+let something : any;        // 该版本和书上有差异，如果不显示调用 any 后面的setName会报错
+something = "Seven";
+something = 7;
+something = "9";
+something.setName("MartySa");
+
+let unionVal : string | number;
+unionVal = "Seven";
+unionVal = 7;
+//unionVal = false;
+function GetLenth(something : string | number) : string
+{
+    return something.toString();
+}
+
+// 接口 警报
+interface Alarm
+{
+    fuc() : void
+}
+
+// 接口 灯光
+interface Light
+{
+    lightON() : void;
+    lightOff() : void;
+}
+
+// 接口 灯光警报，继承接口Alarm
+interface LightableAlarm extends Alarm
+{
+    lightOn() : void;
+    lightOff() : void;
+}
+
+// 类 门
+class Door
+{
+    ShowName() : void
+    {
+        
+    } 
+}
+
+// 多重继承
+class SecurityDoor extends Door implements Alarm
+{
+    fuc() : void
+    {
+
+    }
+}
+
+let door : SecurityDoor = new SecurityDoor; // 不确定是否这样初始化
+door.fuc();
+
+// 汽车，实现接口 Alarm和Light
+class Car implements Alarm, Light
+{
+    lightON() : void
+    {
+
+    }  
+    
+    lightOff() : void 
+    {
+
+    }
+
+    fuc() : void
+    {
+
+    }
+}
+
+class Car2 implements LightableAlarm
+{
+    fuc() : void
+    {
+
+    }
+
+    lightOff() : void 
+    {
+
+    }
+
+    lightOn() : void 
+    {
+        
+    }
+}
+
